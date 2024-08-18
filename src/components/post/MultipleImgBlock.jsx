26bbfc6e052c779e47ea0901ac50875fface03b2
Nelson Hsu <nelsonhsu1027@gmail.com>
2024-08-18 17:07:47 +0800

    更新Hao2照片

--- src/components/post/MultipleImgBlock.jsx
+++ src/components/post/MultipleImgBlock.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 export function MultipleImgBlock({imgList, isOriginSize = false}) {
+  // 2張圖片時，不縮小
+  // 3張圖片時，才縮一樣大小
+  // 可自由代入isOriginSize，選擇是否更新大小
   const colAmount = imgList.length > 2 ? 'sm:grid-cols-3' : 'sm:grid-cols-2';
 
   return <div class={`grid grid-cols-1 ${colAmount} justify-center gap-y-2 sm:gap-x-5 w-full`}>
     {imgList.map((src, index) => (
-      <img key={index} src={src} alt={`Image ${index + 1}`} class={isOriginSize ? '' : 'post-mul-img'} />
+      <img key={index} src={src} alt={`Image ${index + 1}`} class={isOriginSize ||imgList.length === 2  ? '' : 'post-mul-img'} />
   ))}</div>;
 }
 
