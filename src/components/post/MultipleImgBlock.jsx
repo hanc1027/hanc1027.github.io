d7028453cb53cf50bad7189567de0137a52490d4
Nelson Hsu <nelsonhsu1027@gmail.com>
2024-08-18 16:30:21 +0800

    多組照片排組時，設定同樣大小。若多組照片需要原尺寸，可設定參數

--- src/components/post/MultipleImgBlock.jsx
+++ src/components/post/MultipleImgBlock.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export function MultipleImgBlock({imgList}) {
+export function MultipleImgBlock({imgList, isOriginSize = false}) {
   const colAmount = imgList.length > 2 ? 'sm:grid-cols-3' : 'sm:grid-cols-2';
 
   return <div class={`grid grid-cols-1 ${colAmount} justify-center gap-y-2 sm:gap-x-5 w-full`}>
     {imgList.map((src, index) => (
-      <img key={index} src={src} alt={`Image ${index + 1}`} className="w-full h-auto object-cover" />
+      <img key={index} src={src} alt={`Image ${index + 1}`} class={isOriginSize ? '' : 'post-mul-img'} />
   ))}</div>;
 }
 
