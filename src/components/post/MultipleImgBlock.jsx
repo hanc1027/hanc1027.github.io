9617345d31d48c2d514f3531303f368af31cd07f
Nelson Hsu <nelsonhsu1027@gmail.com>
2025-01-30 20:06:43 +0800

    新增 【蘭嶼予我】EP1 - 緣份牽引至飛魚故鄉

--- src/components/post/MultipleImgBlock.jsx
+++ src/components/post/MultipleImgBlock.jsx
@@ -6,13 +6,13 @@ export function MultipleImgBlock({imgList, isOriginSize = false, altText=""}) {
   // 可自由代入isOriginSize，選擇是否更新大小
   const colAmount = imgList.length === 2  || imgList.length === 4  ? 'sm:grid-cols-2' : 'sm:grid-cols-3';
 
-  return <><div class={`grid grid-cols-1 ${colAmount} justify-center gap-y-2 sm:gap-x-5 w-full`}>
+  return <><div className={`grid grid-cols-1 ${colAmount} justify-center items-stretch gap-y-2 sm:gap-x-5 w-full`}>
     {imgList.map((src, index) => (
-      <img key={index} src={src} alt={`Image ${index + 1}`} class={isOriginSize ||imgList.length === 2 || imgList.length === 4 ? '' : 'post-mul-img'} />
+      <img key={index} src={src} alt={`Image ${index + 1}`} className={isOriginSize ||imgList.length === 2 || imgList.length === 4 ? '' : 'post-mul-img'} />
 
   ))}</div>
 
-  { altText !== "" && <div class="w-full flex justify-center mt-2 text-skin-altText  !text-sm"> {altText} </div>}
+  { altText !== "" && <div className="w-full flex justify-center mt-2 text-skin-altText  !text-sm"> {altText} </div>}
 
   <br/></>;
 }
